54e50e552434973e9ddc77516bbdad07cce9987e
camilla alves <camillaalves285@gmail.com>
2023-04-27 21:27:34 -0300

    alterado a cor de fundo o title e bordar dos cards

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -2,7 +2,6 @@ import Container from 'react-bootstrap/Container'
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
 import NavDropdown from 'react-bootstrap/NavDropdown'
-import { Arts } from '../../pages/arts/Arts'
 
 import S from './styles.module.scss'
 import { Link } from 'react-router-dom'
